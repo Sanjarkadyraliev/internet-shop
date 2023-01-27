601e4db9d1c1d2877d2f8b5494ab498af85fd86b
Sanjar <sanzharkadyarliev@gmail.com>
2023-01-27 10:56:10 +0600

    first commit

---
+++ e_store/clients/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Profile
+
+
+def profile(request, user_id):
+    profile = Profile.objects.get(id = user_id)
+    context = {'profile': profile}
+    return render(request, 'clients/profile.html')
